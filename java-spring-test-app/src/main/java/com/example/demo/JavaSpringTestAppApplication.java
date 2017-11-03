82a7996e3c2cc3373cfafa49147cb150babc0975
Philipp Töpfer <philipp.toepfer@arxes-tolina.de>
2017-11-03 12:51:51 +0100

    Basic Spring Boot Setup

---
+++ java-spring-test-app/src/main/java/com/example/demo/JavaSpringTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTestAppApplication.class, args);
+	}
+}
